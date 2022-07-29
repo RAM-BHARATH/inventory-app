2a859b0838f27d2b50acd6cbe28da69c60cecda8
RAM-BHARATH <ramtherapid007@gmail.com>
2022-07-29 08:38:39 +0530

    Added routes

---
+++ routes/inventory.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+
+const product_controller = require('../controllers/productController');
+const category_controller = require('../controllers/categoryController');
+const character_controller = require('../controllers/characterController');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Toy Store' });
+});
+
+router.get('/product/create', product_controller.product_create_get);
+
+router.post('/product/create', product_controller.product_create_post);
+
+router.get('/product/:id/delete', product_controller.product_delete_get);
+
+router.post('/product/:id/delete', product_controller.product_delete_post);
+
+router.get('/product/:id/update',product_controller.product_update_post)
+
+router.post('/product/:id/update',product_controller.product_update_post)
+
+router.get('/category/create', category_controller.category_create_get);
+
+router.post('/category/create', category_controller.category_create_post);
+
+router.get('/category/:id/delete', category_controller.category_delete_get);
+
+router.post('/category/:id/delete', category_controller.category_delete_post);
+
+router.get('/category/:id/update', category_controller.category_update_get);
+
+router.post('/category/:id/update', category_controller.category_update_post);
+
+router.get('/character/create', character_controller.character_create_get);
+
+router.post('/character/create', character_controller.character_create_post);
+
+router.get('/character/:id/delete', character_controller.character_delete_get);
+
+router.post('/character/:id/delete', character_controller.character_delete_post);
+
+router.get('/character/:id/update', character_controller.character_update_get);
+
+router.post('/character/:id/update', character_controller.character_update_post);
+
+module.exports = router;
